78cfbba4956e1953dcdf4ec92d437a23200f76e7
Benjamin Bledsoe <bledsoedesigns@gmail.com>
2016-06-15 16:39:04 -0500

    Init categories page

---
+++ pages/event_categories.php
@@ -0,0 +1,28 @@
+<?php 
+require_once '../templates/navbar.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="keywords" content="ben" />
+	<meta name="description" content="Ben." />
+	<meta name="author" content="Ben" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no" />
+
+	<link type="text/css" rel="stylesheet" href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'/>
+
+	<title>Add Staff</title>
+
+</head>
+
+<body>
+	<div role="document" class="container-fluid">
+<?php print $navbar ?>
+	
+	</div>
+
+	<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>	
+</body>
