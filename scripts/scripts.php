ce3199d17aef30f67444907bb1dab49236ea1237
Benjamin Bledsoe <bledsoedesigns@gmail.com>
2016-06-21 09:50:43 -0500

    Added scripts.php, contains common functions across pages

---
+++ scripts/scripts.php
@@ -0,0 +1,70 @@
+<?php
+
+function generate_select( $fields, $inputName){
+	$html = '<select class="form-control" name="event['.$inputName.']"><option value="">Select One</option>';
+	$selected = '';
+	foreach($fields as $field){
+		if(isset($_POST['event'][$inputName]) && $_POST['event'][$inputName] == $field){
+			$selected = 'selected="selected"';
+		} elseif(isset($GLOBALS['eventToEdit'][$inputName]) && $GLOBALS['eventToEdit'][$inputName] == $field){
+			$selected = 'selected="selected"';
+		}
+		$html .= "<option value='$field' $selected >$field</option>";
+	}
+	$html .='</select>';
+
+	return $html;
+}
+
+function generate_checkbox($key,$value){
+	$checked = '';
+	if(isset($_POST['event']['categories']) && in_array($key, $_POST['event']['categories'])){
+		$checked = 'checked="checked"';
+	} elseif(isset($GLOBALS['eventToEdit']['categories']) && in_array($key, $GLOBALS['eventToEdit']['categories'])){
+		$checked = 'checked="checked"';
+	}
+	$html = '<label class="form-control"> '.$value.' <input type="checkbox" name="event[categories][]" value="'.$key.'" '.$checked.' /></label>';
+	return $html;
+}
+
+// get the existing value for a field if it exists
+function get_existing_field($fieldName){
+	$value = '';
+	if(isset($_POST['event'][$fieldName])){
+		$value = $_POST['event'][$fieldName];
+	} elseif(!empty($GLOBALS['eventToEdit'])){
+		$value = ($fieldName == 'start_date' || $fieldName == 'end_date') ? date('m/d/Y', $GLOBALS['eventToEdit'][$fieldName]->sec) : $GLOBALS['eventToEdit'][$fieldName];
+	}
+	return $value;
+}
+
+function validateId($id){
+	$valid = true;
+	try {
+		$id = new MongoId($id);
+	}	catch (MongoException $e) {
+		$valid = false;
+	}
+	
+	return $valid;
+}
+
+function save($collectionName, $id, $action, $record){
+	$success = true;
+	
+	// update
+	if(!empty($id)){
+		$resp = MDB::update($collectionName, array('_id'=>$id), array('$set'=>$record));;
+	} elseif($action != 'delete') {
+	// default
+		$resp =  MDB::insert($collectionName, $record);
+	}
+	
+	if($resp['error']){
+		$success = false;
+	} 
+	
+	return $success;
+}
+
+?>
